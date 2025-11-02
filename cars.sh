25f53c61f975d7f1e8769fbdfc5f134edbbe5006
Rhys Lindhorst <lindhorst@chapman.edu>
2025-11-02 12:27:39 -0800

    cars.sh and my_old_cars

---
+++ cars.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+# Script name: cars.sh
+# Author: Your Name
+
+my_file="my_old_cars"
+touch "$my_file"
+
+while true; do
+    echo "Please choose an option:"
+    echo "1 - Enter a new car"
+    echo "2 - Display the list of cars"
+    echo "3 - Quit"
+
+    # Read choice (works with redirected input too)
+    if ! read choice; then
+        echo "EOF reached. Exiting."
+        break
+    fi
+
+    case $choice in
+        1)
+            if ! read year; then break; fi
+            if ! read make; then break; fi
+            if ! read model; then break; fi
+            echo "${year}:${make}:${model}" >> "$my_file"
+            ;;
+        2)
+            if [[ -s $my_file ]]; then
+                echo "Cars list (sorted by year):"
+                sort -t: -k1,1 "$my_file"
+            else
+                echo "No cars entered yet."
+            fi
+            ;;
+        3)
+            echo "Goodbye!"
+            break
+            ;;
+        *)
+            echo "Invalid choice, please enter 1, 2, or 3."
+            ;;
+    esac
+done
